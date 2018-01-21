40dff0b1d131b731b5a30dab267aa08278f44c5f
lyonsa <allyons16@gmail.com>
2018-01-21 17:46:33 -0500

    setup models

---
+++ models/user.js
@@ -0,0 +1,39 @@
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        // Giving the Author model a name of type STRING
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {len:[1]}
+        },
+        pass: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {len:[1]}
+        },
+        points: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        role: {
+            type: DataTypes.STRING,
+            defaultValue: 'User'
+        }},{
+        timestamps: false
+    });
+    User.associate = function (models) {
+        // Associating Author with Posts
+        // When an Author is deleted, also delete any associated Posts
+        User.hasMany(models.Page, {
+            onDelete: "cascade"
+        });
+    };
+
+    User.associate = function (models) {
+        User.hasMany(models.Comment, {
+            onDelete: "cascade"
+        });
+    };
+
+    return User;
+};
