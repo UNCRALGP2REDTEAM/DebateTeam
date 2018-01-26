4770f18656ee2bc93f0114bfeb4804c937f466f0
lyonsa <allyons16@gmail.com>
2018-01-25 20:06:04 -0500

    make changes to api routes

--- routes/page-api-routes.js
+++ routes/page-api-routes.js
@@ -13,67 +13,32 @@ var db = require("../models");
 module.exports = function (app) {
 
     // GET route for getting all of the posts
-    app.get("/api/pages", function (req, res) {
-    //app.get("/api/pages", passport.authenticate('jwt', { session: false }), function (req, res) {
-        var query = {};
-        if (req.query.user_id) {
-            query.UserId = req.query.user_id;
-        }
+    app.get("/api/pages_u", function (req, res) {
         // 1. Add a join here to include all of the Authors to these posts
         db.Page.findAll({
-            include: [db.User],
-            where: query
+            include: { 
+                model: db.User,
+                attributes: { exclude: ['pass'] }  
+            },
+            order: db.sequelize.literal('createdAt DESC')
         }).then(function (dbPage) {
             res.json(dbPage);
         });
     });
 
     // Get rotue for retrieving a single post
-    app.get("/api/pages/:id", function (req, res) {
+    app.get("/api/page/:id", function (req, res) {
     //app.get("/api/pages/:id", passport.authenticate('jwt', { session: false }), function (req, res) {
         // 2. Add a join here to include the Author who wrote the Post
         db.Page.findOne({
-            include: [db.User],
-            where: {
-                id: req.params.id
-            }
+            include: {
+                model: db.User,
+                attributes: ['username'],
+            },
+            where: id = req.params.id
         }).then(function (dbPage) {
             console.log(dbPage);
             res.json(dbPage);
         });
-    });
-
-    // POST route for saving a new post
-    app.post("/api/pages", function (req, res) {
-    //app.post("/api/pages", passport.authenticate('jwt', { session: false }), function (req, res) {
-        db.Page.create(req.body).then(function (dbPage) {
-            res.json(dbPage);
-        });
-    });
-
-    // DELETE route for deleting posts
-    app.delete("/api/pages/:id", function (req, res) {
-    //app.delete("/api/pages/:id", passport.authenticate('jwt', { session: false }), function (req, res) {
-        db.Page.destroy({
-            where: {
-                id: req.params.id
-            }
-        }).then(function (dbPage) {
-            res.json(dbPage);
-        });
-    });
-
-    // PUT route for updating posts
-    app.put("/api/pages", function (req, res) {
-    //app.put("/api/pages", passport.authenticate('jwt', { session: false }), function (req, res) {
-        db.Page.update(
-            req.body,
-            {
-                where: {
-                    id: req.body.id
-                }
-            }).then(function (dbPage) {
-                res.json(dbPage);
-            });
     });
 };
