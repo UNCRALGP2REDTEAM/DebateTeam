0c97ef2a314ca5b7601ff5c7519fcc4783d8ac25
Adrian Schmitz <scegad@scegad.io>
2018-01-27 11:34:54 -0500

    Updated login page to submit to login endpoint, and store user token in local storage. Added auth.js with auth helper functions for getting current user info and logging user out.

---
+++ public/js/auth.js
@@ -0,0 +1,18 @@
+// These helper functions can be called from any page JS. 
+
+// Get the currently-logged-in user info. We return null if no user is logged in.
+
+function getCurrentUser() {
+  var userToken = null;
+  if (localStorage.currentUser) {
+    userToken = localStorage.getItem('currentUser');
+  }
+  return userToken;
+}
+
+// Log the user out, by deleting the token form local storage
+function logout() {
+  if (localStorage.currentUser) {
+    localStorage.removeItem('currentUser');
+  }
+}
