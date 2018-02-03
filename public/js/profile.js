0cdd5fe0491b8978ea9c04a12f66f4904e995ab2
lyonsa <allyons16@gmail.com>
2018-02-02 22:08:00 -0500

    create profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,9 +1,32 @@
 $(document).ready(function() {
 	
-	$.ajax("api/users/", {
-		type: "GET",
-	}).then(function(res) {
-		console.log(res);
-		$("#profileInfo").append(res.body);
-	});
+	var currentUser = getCurrentUser();
+	console.log(currentUser);
+	var uId = currentUser.user_id
+	$("#username").text(currentUser.username);
+	if (currentUser.user_id !== null) {
+
+		$.get("/api/pages/u/" + uId, function (data) {
+			console.log("Fetched page info: " + JSON.stringify(data));
+			if (data.length>0){
+				$('#pages').empty();
+				
+				for (var i = 0; i < data.length; i++) {
+					var page = data[i];
+					$('#pages').append('<a href="debate_'+page.id+'">'+page.name+'</a><br>')
+				}
+			}
+		});
+		$.get("/api/comments/u/"+uId, function(data) {
+			console.log("Fetched page info: " + JSON.stringify(data));
+			if (data.length > 0) {
+				$('#comments').empty();
+
+				for (var i = 0; i < data.length; i++) {
+					var comment = data[i];
+					$('#comments').append('<a href="debate_' + comment.PageId + '">'+ comment.text + '</a><br>')
+				}
+			}
+		})
+	}
 });
